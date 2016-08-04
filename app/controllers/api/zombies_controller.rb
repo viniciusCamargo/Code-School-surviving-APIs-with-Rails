34a1ec8967a578487a106f5771435fa38bc85eeb
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-04 00:32:48 -0300

    Inclui alteracao da classe Api para API no inflections.rb

--- app/controllers/api/zombies_controller.rb
+++ app/controllers/api/zombies_controller.rb
@@ -1,4 +1,10 @@
-module Api
+# module Api
+module API
   class ZombiesController < ApplicationController
   end
 end
+
+# In Rails, all classes must be in camel case in order to run, but we are
+# used to acronyms all in caps, if we simply rename 'module Api' to
+# 'module API' it won't work. So we have to add an entry to the inflections
+# file: /config/initializers/inflections.rb
