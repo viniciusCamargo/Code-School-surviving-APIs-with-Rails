20a16dca06be6d633d8696ac6f44799e0d09ffdc
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-04 00:04:53 -0300

    Apenas converte tabs para espacos

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   # resources :zombies
   # resources :zombies, only: :index
   constraints subdomain: 'api' do
-	  namespace :api, path: '/' do
-	  	resources :zombies, only: [:index, :show]
-	  	resources :humans, except: [:destroy, :edit, :update]
-	  end
-	end
-	# # ou
-	# namespace :api, path: '/', constraints: { subdomain: 'api' } do
- #  	resources :zombies, only: [:index, :show]
- #  	resources :humans, except: [:destroy, :edit, :update]
-	# end
+    namespace :api, path: '/' do
+      resources :zombies, only: [:index, :show]
+      resources :humans, except: [:destroy, :edit, :update]
+    end
+  end
+  # # ou
+  # namespace :api, path: '/', constraints: { subdomain: 'api' } do
+  #   resources :zombies, only: [:index, :show]
+  #   resources :humans, except: [:destroy, :edit, :update]
+  # end
 
   # Ao usar um namespace, precisamos definir o controller dentro de um
   # module, nesse caso 'module Api'; em uma pasta com o mesmo nome do
@@ -23,17 +23,17 @@ Rails.application.routes.draw do
   # api.meusite.com/zombies
 
   # with_options only: :index do |list|
-  # 	list.resources :zombies
-  # 	list.resources :humans
-  # 	list.resources :medical_kits
+  #   list.resources :zombies
+  #   list.resources :humans
+  #   list.resources :medical_kits
   # end
 
   # resources :zombies, constraints: { subdomain: 'api' }
   # resources :humans, constraints: { subdomain: 'api' }
   # # ou
   # constraints subdomain: 'api' do
-  # 	resources :zombies
-  # 	resources :humans
+  #   resources :zombies
+  #   resources :humans
   # end
 
   # # Notas: https://gist.github.com/viniciusCamargo/be67420b7638685c9f27e213b79ff896
