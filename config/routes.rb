f7cae32620597f7facfc2dae000989037fece8b0
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-03 23:21:47 -0300

    Inclui constraints pra rota rodar no subdominio 'api'

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,14 @@ Rails.application.routes.draw do
   # 	list.resources :humans
   # 	list.resources :medical_kits
   # end
-  
+
+  # resources :zombies, constraints: { subdomain: 'api' }
+  # resources :humans, constraints: { subdomain: 'api' }
+  # # ou
+  # constraints subdomain: 'api' do
+  # 	resources :zombies
+  # 	resources :humans
+  # end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
