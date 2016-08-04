450238dde72629bd6d0b6a233a25dfb5ba2c9032
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-03 23:11:01 -0300

    Cria resources zombies e humans

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :zombies
+  # resources :zombies
+  # resources :zombies, only: :index
+  resources :zombies, only: [:index, :show]
+  resources :humans, except: [:destroy, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
