9af11a1e399ef57478228a822e92bcc70bca9e9f
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-08-03 23:14:49 -0300

    Rotas com um exemplo do metodo with_options

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   # resources :zombies, only: :index
   resources :zombies, only: [:index, :show]
   resources :humans, except: [:destroy, :edit, :update]
+
+  # with_options only: :index do |list|
+  # 	list.resources :zombies
+  # 	list.resources :humans
+  # 	list.resources :medical_kits
+  # end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
